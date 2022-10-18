3b6ac436154d92c598a19b74724ee346d50102c6
Suhaeeb <52637002+Suhaeeb@users.noreply.github.com>
2022-10-18 23:55:20 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,20 @@
+print("Suhaeeb")
+
+name = ("suhaeeb")
+age = "18"
+number = "07500171616"
+
+
+print("my name is "+(name))
+print("my age is "+(age))
+print("my number is "+number)
+
+print("."
+      "."
+      ".")
+
+print("my name is "+name+
+      " my age is "+age+
+      " my number is "+number)
+
+#    17/10/2022
