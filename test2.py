5105d3742e158c18cb9b1d4b299c08be30874a87
Suhaeeb <52637002+Suhaeeb@users.noreply.github.com>
2022-10-20 00:08:57 +0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,26 @@
+print("hi\nmy name\" suhaeeb\tkhalid")
+name = "suhaeeb"
+num = "07500171616"
+inf = "hello my name is suhaeeb"
+
+
+print(name.upper())
+print(name.lower)
+print(name.islower())
+print(name.isupper())
+
+print(name.upper(),name.lower())
+print(len(num))
+print(num.index("6"))
+print(name.capitalize())
+print(name.__class__)
+
+
+print(inf[0:5])
+print(inf[-1])
+print(inf[-9])
+
+
+print(inf)
+print(inf.replace("hello","hi"))
+
