ffa247b5116ba93360121f06754b159ec213c278
Suhaeeb <52637002+Suhaeeb@users.noreply.github.com>
2022-10-20 06:57:25 +0300

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,48 @@
+#import math
+# awae sarawa pewesta hamu kat math bakar bene bo bakar henane
+
+#form math import *
+#awaean pewest naka math bakar bene
+
+"""
+tb gar bnuse import math dabe baw shewaza benuse
+print(math.pi)
+
+-----------
+
+agar bnuse from math import * baw shewaza dae nuse
+print(pi)
+"""
+
+from math import *
+
+number = -4
+
+# nrxe root (mrxe rut)
+print(abs(number))
+#tuan u hez (pow)
+print(pow(4,3))
+
+#gawratren zhmara (max)
+print(max(1,3,5,7,9,4,))
+#bchuk tren zhmara(min)
+print(min(1,3,4,6,5,7,9,8))
+
+#roun(round)
+print(round(3.4))
+print(round(3.5))
+
+
+
+print(pi)
+
+print(sin(360))
+print(cos(360))
+
+print(log(4))
+
+#rage duja
+print(sqrt(9))
+
+print("zhmara "+str(1))
+print("zhmara "+str(number))
