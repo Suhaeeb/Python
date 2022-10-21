e081236e9ae00b95ae15a56253abd80b84892bd6
Suhaeeb <52637002+Suhaeeb@users.noreply.github.com>
2022-10-21 11:07:30 +0300

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,13 @@
+name = input("enter your name:  ")
+print("slaw "+name)
+
+
+f1 = input("ener first name: ")
+f2 = input("ener second name: ")
+print("slaw "+f1+" "+f2)
+
+
+num1 = int(input("ener first number "))
+num2 = int(input("ener second number "))
+result = num1 + num2
+print(result)
